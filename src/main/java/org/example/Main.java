cd61dd6cf2936f01aaa19464ac69c1edac93f938
daniel cooder <danielcooder@gmail.com>
2024-07-25 18:11:21 -0300

    feat: repeticao com FOR

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int cc=1; cc<=10; cc++) {
+            System.out.println("Cambalhota " +  cc);
+        }
+
+
+    }
+}
